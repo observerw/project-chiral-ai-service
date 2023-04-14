6c3b698b2e09510638eee840d03bd6093c896256
王博伟 <wangbowei.114514@bytedance.com>
2023-04-14 17:53:01 +0800

    修复entityResolver逻辑

--- project_chiral_ai_service/entity/resolver.py
+++ project_chiral_ai_service/entity/resolver.py
@@ -1,3 +1,4 @@
+import math
 from typing import List
 
 from pydantic import BaseModel
@@ -5,7 +6,7 @@ from pydantic import BaseModel
 from project_chiral_ai_service.entity.linker import EntityLinker, CharaItem, LinkOption
 from project_chiral_ai_service.entity.recognizer import EntityRecognizer
 from project_chiral_ai_service.entity.template import EntityPromptParams
-from project_chiral_ai_service.schema import LangType
+from project_chiral_ai_service.schema import LangType, LANG_MAP
 
 
 class EntityResolveReq(BaseModel):
@@ -40,7 +41,8 @@ class EntityResolver:
         for name in names:
             options = self.linker.process(table=params.table, name=name, lang=params.lang)
 
-            if len(options) == 1:
+            # 分数为1，完全匹配
+            if any([math.fabs(option.score - 1) < 1e-6 for option in options]):
                 result.resolved.append(Resolved(
                     id=options[0].id
                 ))
@@ -49,6 +51,9 @@ class EntityResolver:
                     name=name,
                     options=options
                 ))
+
+        # 去重
+        result.resolved = list({item.id: item for item in result.resolved}.values())
 
         return result
 
@@ -64,8 +69,8 @@ if __name__ == "__main__":
             CharaItem(id=2, name='大道寺知世', alias=[]),
             CharaItem(id=3, name='可鲁贝洛斯', alias=['小可']),
         ],
-        lang='cn',
-        doc="""小樱做了不可思议的预知梦。在梦中，小樱面对的是谁……等待凭借预知梦走向东京铁塔的小樱等人的是……友枝町突然被黑暗笼罩，街上的人们都睡了过去。清晨之前若不能打破魔法，人们就会如此沉睡不醒。小樱和可鲁贝洛斯决定前往友枝町，但是在路上遇到了知世。知世说他也是为了打破魔法而来，但是他的目的是为了拯救自己的妹妹。小樱和可鲁贝洛斯决定与知世一起前往友枝町。"""
+        lang=LANG_MAP['cn'],
+        doc="""小樱做了不可思议的预知梦。在梦中，小樱面对的是谁……等待凭借预知梦走向东京铁塔的小樱和小可等人的是……友枝町突然被黑暗笼罩，街上的人们都睡了过去。清晨之前若不能打破魔法，人们就会如此沉睡不醒。小樱和可鲁贝洛斯决定前往友枝町，但是在路上遇到了知世。知世说他也是为了打破魔法而来，但是他的目的是为了拯救自己的妹妹。小樱和可鲁贝洛斯决定与知世一起前往友枝町。"""
     ))
 
     print(result)
